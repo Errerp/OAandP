44219410e68cfde5d5fa060f28cc9ffb41a374f6
Errerp <113059470+Errerp@users.noreply.github.com>
2022-10-19 17:29:09 +0300

    Update 3.c

--- lab#3/3.c
+++ lab#3/3.c
@@ -2,42 +2,57 @@
 
 int main()
 {
-    int N, i, j, num, arr[100];
+    int N, i,mass, j, num, arr[100];
     int CurrentCounter = 0, BiggestCounter = 0;
+    srand(time(0));
     printf("Input number of array elements\n");
     if (scanf_s("%d", &N) != 1)
     {
-        printf("Error");
+        printf("Input error");
+        return 0;
     }
-    else
+    printf("Select the array input type\n1-Random\n2-Own\n");
+    scanf_s("%d", &mass);
+    switch (mass)
     {
-       printf("Input array elements\n");
-            for (i = 0; i < N; i++)
+    case 1:
+        for (i = 0; i < N; i++) {
+            arr[i] = rand() % 100;
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+        break;
+    case 2:
+        printf("Input array elements\n");
+        for (i = 0; i < N; i++)
+        {
+            if (scanf_s("%d", &arr[i]) != 1)
             {
-                if (scanf_s("%d", &arr[i]) != 1)
-                {
-                    printf("Error");
-                    return 0;
-                }
+                printf("Input error");
+                return 0;
             }
-                for (int i = 0; i < N; i++)
-                {
-                    for (int j = i; j < N; j++)
-                    {
-                        if (arr[i] == arr[j])
-                        {
-                         CurrentCounter++;
-                        }
-                    }
-                    if (CurrentCounter > BiggestCounter)
-                    {
-                        BiggestCounter = CurrentCounter;
-                        num = arr[i];
-                    }
-                    CurrentCounter = 0;
-                }
-            printf("Element = %d\n", num);
-   
+        }
+        break;
+    default:
+        printf("Input error");
+        return 0;
     }
+        for (int i = 0; i < N; i++)
+        {
+                for (int j = i; j < N; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        CurrentCounter++;
+                    }
+                }
+                if (CurrentCounter > BiggestCounter)
+                {
+                    BiggestCounter = CurrentCounter;
+                    num = arr[i];
+                }
+                    CurrentCounter = 0;
+        }
+            printf("Element = %d\n", num);
     return 0;
 }
