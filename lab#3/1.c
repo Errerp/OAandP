25858371e6af4b95363370c15a6e010fe55abcea
Errerp <113059470+Errerp@users.noreply.github.com>
2022-10-19 17:31:02 +0300

    Update 1.c

--- lab#3/1.c
+++ lab#3/1.c
@@ -2,29 +2,46 @@
 
 int main()
 {
-    int N, i, large = 0, arr[100], C, proizv = 1, max = 0;
+    int N, i,mass, large = 0, arr[100], C, proizv = 1, max = 0;
+    srand(time(0));
     printf("Input number of array elements\n");
     if (scanf_s("%d", &N) != 1)
     {
-        printf("Error");
+        printf("Input error");
+        return 0;
     }
-    else
+    printf("Select the array input type\n1-Random\n2-Own\n");
+    scanf_s("%d", &mass);
+    switch (mass)
     {
+    case 1:
+        for (i = 0; i < N; i++) {
+            arr[i] = rand() % 100;
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+        break;
+    case 2:
         printf("Input array elements\n");
         for (i = 0; i < N; i++)
         {
             if (scanf_s("%d", &arr[i]) != 1)
             {
-                printf("Error");
+                printf("Input error");
                 return 0;
             }
         }
+        break;
+    default:
+        printf("Input error");
+        return 0;
+    }
         printf("Input C\n");
         if (scanf_s("%d", &C) != 1)
         {
-            printf("Error");
+            printf("Input error");
+            return 0;
         }
-        else {
             for (i = 0; i < N; i++)
             {
                 if (arr[i] > C)
@@ -33,7 +50,6 @@ int main()
                 }
             }
             printf("Number of array elements, large C = %d\n", large);
-
             for (i = 1; i < N; i++)
             {
                 if (arr[i] > arr[max])
@@ -46,7 +62,5 @@ int main()
                 proizv *= arr[i];
             }
             printf("Composition = %d", proizv);
-        }
-    }
     return 0;
 }
