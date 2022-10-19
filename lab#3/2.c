8c70a5440b729c55aa2b52987603c43232388bf7
Errerp <113059470+Errerp@users.noreply.github.com>
2022-10-19 17:30:11 +0300

    Update 2.c

--- lab#3/2.c
+++ lab#3/2.c
@@ -2,52 +2,53 @@
 
 int main()
 {
-    int N, i, k, arr[100], j;
+    int N, i,mass, k, arr[100], j;
+    srand(time(0));
     printf("Input number of array elements\n");
-        if (scanf_s("%d", &N) != 1)
-	    {
-		    printf("Error");
-	    }
-        else 
-        {
-            printf("Input k\n");
-            if (scanf_s("%d", &k) != 1)
-            {
-                printf("Error");
+    if (scanf_s("%d", &N) != 1)
+    {
+        printf("Input error");
+        return 0;
+    }
+    printf("Select the array input type\n1-Random\n2-Own\n");
+    scanf_s("%d", &mass);
+    switch (mass){
+    case 1:
+        for (i = 0; i < N; i++) {
+            arr[i] = rand() % 100;
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+        break;
+    case 2:
+        printf("Input array elements\n");
+        for (i = 0; i < N; i++){
+            if (scanf_s("%d", &arr[i]) != 1){
+                printf("Input error");
+                return 0;
             }
-            else
-            {
-                printf("Input array elements\n");
-                for (i = 0; i < N; i++)
-                {
-                    if (scanf_s("%d", &arr[i]) != 1)
-                    {
-                        printf("Error");
-                        return 0;
-                    }
-                }
-                for (i = 0; i < N; i++)
-                {
-                    printf("%d ", arr[i]);
-                }
-                printf("\n");
-                for (i = N; i > 0; i--)
-                {
-                    if (i % k == 0)
-                    {
+        }
+        break;
+    default:
+        printf("Input error");
+        return 0;
+    }
+    printf("Input k\n");
+            if (scanf_s("%d", &k) != 1){
+                printf("Input error");
+                return 0;
+            }
+            for (i = N; i > 0; i--){
+                    if (i % k == 0){
                         N++;
-                        for (j = N; j > i; j--)
-                        {
+                        for (j = N; j > i; j--){
                             arr[j] = arr[j - 1];
                         }
                         arr[i + 1] = 0;
                     }
-                }
-                for (i = 0; i < N; i++)
-                {
+            }
+                for (i = 0; i < N; i++){
                     printf("%d ", arr[i]);
                 }
-            }
-        }
 	return 0;
 }
