e41227c98814e13283a65a2478d3bf9a8dde54aa
Errerp <113059470+Errerp@users.noreply.github.com>
2022-10-21 18:37:10 +0300

    Update 2.c

--- lab#3/2.c
+++ lab#3/2.c
@@ -2,42 +2,47 @@
 
 int main()
 {
-    int N, i,mass, k, arr[100], j;
+    int N, i, mass, k, j, arr[100];
     srand(time(0));
     printf("Input number of array elements\n");
-    if (scanf_s("%d", &N) != 1)
+    while (scanf_s("%d", &N) != 1)
     {
-        printf("Input error");
-        return 0;
+        printf("Input error. Try again: ");
+        rewind(stdin);
     }
     printf("Select the array input type\n1-Random\n2-Own\n");
-    scanf_s("%d", &mass);
-    switch (mass){
+    while (scanf_s("%d", &mass) != 1)
+    {
+        printf("Input error. Try again: ");
+        rewind(stdin);
+    }
+    switch (mass)
+    {
     case 1:
         for (i = 0; i < N; i++) {
-            arr[i] = rand() % 100;
+            arr[i] = rand() % 200 - 100;
             printf("%d ", arr[i]);
         }
         printf("\n");
         break;
     case 2:
         printf("Input array elements\n");
-        for (i = 0; i < N; i++){
-            if (scanf_s("%d", &arr[i]) != 1){
-                printf("Input error");
-                return 0;
+        for (i = 0; i < N; i++)
+        {
+            while (scanf_s("%d", &arr[i]) != 1)
+            {
+                printf("Input error. Try again: ");
+                rewind(stdin);
             }
         }
         break;
-    default:
-        printf("Input error");
-        return 0;
     }
     printf("Input k\n");
-            if (scanf_s("%d", &k) != 1){
-                printf("Input error");
-                return 0;
-            }
+    while (scanf_s("%d", &k) != 1)
+    {
+        printf("Input error. Try again: ");
+        rewind(stdin);
+    }
             for (i = N; i > 0; i--){
                     if (i % k == 0){
                         N++;
