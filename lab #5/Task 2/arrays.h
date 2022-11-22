d096105339374a45c1aabc3193eda6a1d56963fd
Errerp <113059470+Errerp@users.noreply.github.com>
2022-11-22 17:31:19 +0300

    Update arrays.h

--- lab #5/Task 2/arrays.h
+++ lab #5/Task 2/arrays.h
@@ -2,58 +2,65 @@
 #include <time.h>
 #include <stdio.h>
 
+//выделение памяти под матрицу
+int* memory(int n, int m)
+{
+	int **matr = (int**)malloc(n * sizeof(int*));
+	for (int i = 0; i < n; i++)
+		*(matr + i) = (int*)calloc(m, sizeof(int));
+	return matr;
+}
 // печать матрицы
-void print_row(int* arr, int n)
+void print_matr(int** matr, int n,int m)
 {
-    for (int i = 0; i < n; i++)
-        printf("%5d", arr[i]); // arr[i]
-    printf("\n");
-}
-void print_matr(int** matr, int n, int m)
-{
-    for (int i = 0; i < n; i++)
-        print_row(matr[i], m);
-
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%5d", *(*(matr + i) + j));
+        }
+        printf("\n");
+    }
 }
 
-// ввод элементов матрицы
-void input_row(int* arr, int n)
-{
-    //srand(time(NULL));
-    for (int i = 0; i < n; i++)
-        *(arr + i) = rand() % 10;
-}
-
+// ввод элементов матрицы c клавиатуры
 void input_matr(int** matr, int n, int m)
 {
-    int i, j;
-    for (i = 0; i < n; i++)
-        input_row(*(matr + i), m);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("input element [%d][%d]\n", i + 1, j + 1);
+			while (scanf_s("%d", *(matr + i) + j) != 1) {
+				printf("input error\n");
+				rewind(stdin);
+			}
+		}
+	}
+}
+// ввод элементов матрицы рандомом
+void rand_matr(int** matr, int n, int m) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			*(*(matr + i) + j) = rand() % 6;
+		}
+	}
 }
 // работа в матрице
-int** work_in_mat(int* arr, int n, int m)
+void work_in_matr(int** matr, int n, int m)
 {
-    int i, j, sum, max = 0, num = 0;
-   for (j = 0; j < m; j++) {
-        sum = 0;
-        for (i = 0; i < n; i++) {
-            sum += *(arr + i * n + j);
-        }
-        if (sum > max) {
-            max = sum;
-            num = j;
-        }
-    }
-    for (j = num; j < m; j++) {
-        for (i = 0; i < n; i++)
-        {
-            *(arr + i * n + j) = *(arr + i * n + j + 1);
-        }
-    }
-    m--;
-    arr = (int**)realloc(arr, m * sizeof(int*));
-    for (int i = 0; i < n; i++) {
-        *(arr + i) = (int*)realloc(arr,m * sizeof(int));
-    }
-    return arr;
+	int max = 0, colmax = 0;
+	for (int j = 0; j < m; j++) {
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum += *(*(matr + i) + j);
+			if (sum > max) {
+				max = sum;
+				colmax = j;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+
+		for (int j = colmax; j < m - 1; j++) {
+			*(*(matr + i) + j) = *(*(matr + i) + j + 1);
+		}
+		*(matr + i) = realloc(*(matr + i), (m - 1) * sizeof(int));
+	}
 }
