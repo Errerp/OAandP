c17ab4527c08d20dbd5666dce78d615a9dac9ed7
Errerp <113059470+Errerp@users.noreply.github.com>
2022-11-22 17:31:35 +0300

    Update 2.c

--- lab #5/Task 2/2.c
+++ lab #5/Task 2/2.c
@@ -1,51 +1,58 @@
 /*
     В двумерной матрице размером NxM найти столбец 
-    с наибольшей суммой элементов и удалить его.
+    с наибольшей суммой элементов и удалить его.(удаляет первый если несколько)
 */
 #include "arrays.h"
-
-void inputn(int* n) {
-    int x;
-    while (scanf_s("%d", &x) != 1 || x > 100 || x < 1 || x % 1 != 0 || getchar() != '\n') {
-        printf("Error. Try again: ");
-        rewind(stdin);
-    }
-    *n = x;
+void inputrow(int* m) {
+	int x;
+	while (scanf_s("%d", &x) != 1 || x > 100 || x < 1 || x % 1 != 0 || getchar() != '\n') {
+		printf("Error. Try again: ");
+		rewind(stdin);
+	}
+	*m = x;
 }
-void inputm(int* m) {
-    int x;
-    while (scanf_s("%d", &x) != 1 || x > 100 || x < 1 || x % 1 != 0 || getchar() != '\n') {
-        printf("Error. Try again: ");
-        rewind(stdin);
-    }
-    *m = x;
+void inputcol(int* n) {
+	int x;
+	while (scanf_s("%d", &x) != 1 || x > 100 || x < 1 || x % 1 != 0 || getchar() != '\n') {
+		printf("Error. Try again: ");
+		rewind(stdin);
+	}
+	*n = x;
 }
-
-int** memory(int n, int m)
-{
-    int** arr;
-    arr = (int**)malloc(sizeof(int*) * n);
-    for (int i = 0; i < n; i++){
-        *(arr + i) = (int*)malloc(sizeof(int) * m);
-    }
-    return arr;
+int chooseinput(int *matr,int n,int m) {
+	int x;
+	while (scanf_s("%d", &x) != 1 || x > 100 || x < 1 || x % 1 != 0 || getchar() != '\n') {
+		printf("Error. Try again: ");
+		rewind(stdin);
+	}
+	switch (x)
+	{
+	case 1:
+		printf("\nInput array elements: \n");
+		input_matr(matr, n, m);
+		break;
+	case 2:
+		rand_matr(matr, n, m);
+		break;
+	}
+	return matr;
 }
-
-int main()
-{
-    int n, m = 0, mt, *arr;
-    srand(time(NULL));
-    printf("Input the size row: ");
-        inputn(&n);
-    printf("Input the size column: ");
-        inputm(&m);
-        arr = memory(n,m);
-    input_matr(arr, n, m);
-        printf("\nSource array: \n");
-            print_matr(arr, n, m);
-            arr = work_in_mat(arr, n, m);
-            m--;
-    printf("\nThe resulting array: \n");
-            print_matr(arr, n, m);
-   return 0;
+void main() {
+	int  m, n, ** matr;
+	srand(time(NULL));
+	printf("Input the size column: ");
+		inputcol(&n);
+	printf("Input the size row: ");
+		inputrow(&m);
+		matr = memory(n,m);
+	printf("Select the array input type:\n1)Own\n2)Random\n");
+		chooseinput(matr,n,m);
+	printf("\nSource array: \n");
+		print_matr(matr, n, m);
+		work_in_matr(matr, n, m);
+	m--;
+	printf("\nThe resulting array: \n");
+		print_matr(matr, n, m);
+		free(matr);
+	printf("\n");
 }
