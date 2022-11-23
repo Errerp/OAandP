59739aed3c63d36d480eb4d06a33bd2b5558ab0f
Errerp <113059470+Errerp@users.noreply.github.com>
2022-11-23 17:14:35 +0300

    Update function.c

--- lab #6/function.c
+++ lab #6/function.c
@@ -9,12 +9,14 @@ void* libertadarr(int* arr);
 void printarr(int* arr, int n);
 void inputarr(int* arr, int n);
 void randarr(int* arr, int n);
+void poloz(int* arr, int n, int* p);
 void merge(int* arr, int n, int* t1);
 void badmerge(int* arr, int n);
 void bubble(int* arr, int n, int* t2);
-////////////////////////////////////////////////                                                                                                                                  
-void poloz(int* arr, int n, int* p);
-void printmat(int* arr, int n, int m);
-void inputmat(int* arr, int n, int m);
-void randmat(int* arr, int n, int m);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////                                                                                                                                    
+int* memory(int n, int m);
+void* libertad(int n, int** matr);
+void print_matr(int** matr, int n, int m);
+void input_matr(int** matr, int n, int m);
+void rand_matr(int** matr, int n, int m);
 // сортировка методом выбора
