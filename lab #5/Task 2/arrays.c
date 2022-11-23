facf0b0e1ead0dec1178bbe671dab1ea4643333e
Errerp <113059470+Errerp@users.noreply.github.com>
2022-11-23 16:40:31 +0300

    Create arrays.c

---
+++ lab #5/Task 2/arrays.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int* memory(int n, int m);
+void* libertad(int n, int** matr);
+void print_matr(int** matr, int n, int m);
+void input_matr(int** matr, int n, int m);
+void rand_matr(int** matr, int n, int m);
+void work_in_matr(int** matr, int n, int m);
