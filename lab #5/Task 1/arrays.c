fce6738307e41935c441e7a4eccfb4cab2dfcb5b
Errerp <113059470+Errerp@users.noreply.github.com>
2022-11-23 16:31:27 +0300

    Create arrays.c

---
+++ lab #5/Task 1/arrays.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void* memory(int n);
+void* libertad(int* arr);
+void* rememory(int* arr, int n);
+void printarr(int* arr, int n);
+void inputarr(int* arr, int n);
+void workarr(int* arr, int n, int* count);
