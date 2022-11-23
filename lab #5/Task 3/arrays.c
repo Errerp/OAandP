67a966c0e5fe1969537320d6d829cec14470702b
Errerp <113059470+Errerp@users.noreply.github.com>
2022-11-23 17:19:03 +0300

    Update arrays.c

--- lab #5/Task 3/arrays.c
+++ lab #5/Task 3/arrays.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 int* memory(int n, int m);
-void* liberdat(int n, int** matr);
+void* libertad(int n, int** matr);
 void print_matr(int** matr, int n, int m);
 void input_matr(int** matr, int n, int m);
 void rand_matr(int** matr, int n, int m);
