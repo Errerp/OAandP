4d4793f42fc0c2a30a3f67544eb1a7e278f6cbc3
Errerp <113059470+Errerp@users.noreply.github.com>
2022-11-23 17:14:59 +0300

    Update function.h

--- lab #6/function.h
+++ lab #6/function.h
@@ -160,31 +160,47 @@ void bubble(int* arr, int n,int *t2) {
     *t2 = fTimeStop - fTimeStart;
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int* memory(int n, int m)
+{
+    int** matr = (int**)malloc(n * sizeof(int*));
+    for (int i = 0; i < n; i++)
+        *(matr + i) = (int*)calloc(m, sizeof(int));
+    return matr;
+}
+void* libertad(int n, int** matr) {
+    for (int i = 0; i < n; i++)
+        free(*(matr + i));
+    free(matr);
+}
 // печать матрицы
-void printmat(int* arr, int n, int m) {
+void print_matr(int** matr, int n, int m)
+{
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            printf("%5d ", *(arr + i * n + j));
+            printf("%5d", *(*(matr + i) + j));
         }
         printf("\n");
     }
 }
-// ввод элементов матрицы
-void inputmat(int* arr, int n, int m) {
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            while (scanf_s("%d", arr + i * n + j) != 1 || *(arr + i * n + j) % 1 != 0 || getchar() != '\n') {
-                printf("Ошибка. Попробуйте снова: ");
+
+// ввод элементов матрицы c клавиатуры
+void input_matr(int** matr, int n, int m)
+{
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("input element [%d][%d]\n", i + 1, j + 1);
+            while (scanf_s("%d", *(matr + i) + j) != 1) {
+                printf("input error\n");
                 rewind(stdin);
             }
         }
     }
 }
 // ввод элементов матрицы рандомом
-void randmat(int* arr, int n, int m) {
+void rand_matr(int** matr, int n, int m) {
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
-            *(arr + i * n + j) = rand() % 200 - 100;
+            *(*(matr + i) + j) = rand() % 6;
         }
     }
 }
