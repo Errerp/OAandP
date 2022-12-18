71682892fe084b1ca0ee6fbb8ab201b4428c88e5
Errerp <113059470+Errerp@users.noreply.github.com>
2022-12-18 18:12:08 +0300

    Update func.h

--- lab #8/func.h
+++ lab #8/func.h
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include "locale.h"
 
-char* initString(const char* text);
-int stringLength(const char* text);
 int count_chet_words(const char* string);
-void quickSort(char** strings, int left, int right, int(*compareFunc)(char*));
+void quick_sort(char** strings, int left, int right, int(*compareFunc)(char*));
 void type(int argc, char** argv);
