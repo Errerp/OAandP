5208dfda3f558115ed72d2d7eeeb7b539821aaac
Errerp <113059470+Errerp@users.noreply.github.com>
2022-12-20 22:36:06 +0300

    Update Source.c

--- lab #8/Source.c
+++ lab #8/Source.c
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
 		printf("Ошибка.Нет аргумента");
 		return 0;
 	}
-	quick_sort(argv, 0, argc - 1, &count_chet_words);
+	quick_sort(argv, 1, argc - 1, &count_chet_words);
 	printf("Сортировка строк Хоаром:\n");
 	type(argc, argv);
 	return 0;
