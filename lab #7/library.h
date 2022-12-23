d12d9d403bfef0e4e85bdde262bb658309e179a6
Errerp <113059470+Errerp@users.noreply.github.com>
2022-12-23 20:12:42 +0300

    Delete lab #7 directory

--- lab #7/library.h
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "locale.h"
-
-void print(char* str);
-char* rememory(char* str, int size, int sizenew);
-void first_task(char** line, char** line2);
-void second_task(char** line1, char** line2);
