33e20e68c2f5affecc3bb5cafba2b5b54374765c
Errerp <113059470+Errerp@users.noreply.github.com>
2022-12-23 19:57:00 +0300

    Add files via upload

---
+++ Sem 1/Lab 5/Task 3/arrays.c
@@ -0,0 +1,88 @@
+#include "arrays.h"
+
+void input_size(int* n) {
+	int x;
+	while (scanf_s("%d", &x) != 1 || x < 1 || getchar() != '\n') {
+		printf("Ошибка. Попробуйте снова: ");
+		rewind(stdin);
+	}
+	*n = x;
+}
+//выделение памяти под матрицу
+int* memory(int n, int m)
+{
+	int** matr = (int**)malloc(n * sizeof(int*));
+	for (int i = 0; i < n; i++)
+		*(matr + i) = (int*)calloc(m, sizeof(int));
+	return matr;
+}
+void* libertad(int n, int** matr) {
+	for (int i = 0; i < n; i++)
+		free(*(matr + i));
+	free(matr);
+}
+int chooseinput(int* matr, int n, int m) {
+	int x;
+	while (scanf_s("%d", &x) != 1 || x > 100 || x < 1 || x % 1 != 0 || getchar() != '\n') {
+		printf("Ошибка. Попробуйте снова: ");
+		rewind(stdin);
+	}
+	switch (x)
+	{
+	case 1:
+		input_matr(matr, n, m);
+		break;
+	case 2:
+		rand_matr(matr, n, m);
+		break;
+	}
+	return matr;
+}
+// печать матрицы
+void print_matr(int** matr, int n, int m)
+{
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("%5d", *(*(matr + i) + j));
+		}
+		printf("\n");
+	}
+}
+
+// ввод элементов матрицы c клавиатуры
+void input_matr(int** matr, int n, int m)
+{
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("Введите элемент [%d][%d]\n", i + 1, j + 1);
+			while (scanf_s("%d", *(matr + i) + j) != 1) {
+				printf("Ошибка. Попробуйте снова:\n");
+				rewind(stdin);
+			}
+		}
+	}
+}
+// ввод элементов матрицы рандомом
+void rand_matr(int** matr, int n, int m) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			*(*(matr + i) + j) = rand() % 6;
+		}
+	}
+}
+// работа в матрице
+void work_in_matr(int** matr, int n, int m)
+{
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (i + j < n) {
+				printf("%5d", *(*(matr + i) + j));
+			}
+			else {
+				*(matr + i) = realloc(*(matr + i), (m - 1) * sizeof(int));
+				m--;
+			}
+		}
+		printf("\n");
+	}
+}
