bce5059a8b51fab34d51811ba85e5bb243bbcbdb
Errerp <113059470+Errerp@users.noreply.github.com>
2022-12-23 19:57:40 +0300

    Delete lab #8 directory

--- lab #8/func.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "locale.h"
-
-int count_chet_words(const char* string);
-void quick_sort(char** strings, int left, int right, int(*compareFunc)(char*));
-void type(int argc, char** argv);
