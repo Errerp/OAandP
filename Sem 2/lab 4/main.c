723df7b328457c3049630a2b2f6a16f1a1ea1642
Errerp <113059470+Errerp@users.noreply.github.com>
2023-03-25 12:01:44 +0300

    Update main.c

--- Sem 2/lab 4/main.c
+++ Sem 2/lab 4/main.c
@@ -1,18 +1,9 @@
 #include "func.h"
 
 int main() {
-    int task;
-    while(1){
+    int len = 0;
+    Shapes* arr = malloc(sizeof(Shapes));
+    menu(&arr, &len);
 
-        int len = 0;
-        Shapes* arr = malloc(sizeof(Shapes));
-        menu(&arr, &len);
-
-        free(arr);
-        
-        printf("Do u wanna repeat task?\n1.Yes\n2.No\n");
-        input(&task, 1,2);
-        if(task == 2)
-            return 0;
-    }
+    return 0;
 }
