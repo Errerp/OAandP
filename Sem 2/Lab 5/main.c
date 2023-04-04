bcb72686b23fe3193e939a002cc1b5551cbc454e
Errerp <113059470+Errerp@users.noreply.github.com>
2023-04-04 17:42:41 +0300

    Update main.c

--- Sem 2/Lab 5/main.c
+++ Sem 2/Lab 5/main.c
@@ -3,8 +3,11 @@
 int main() {
     int t = 1, choose;
     while(t != 2){
-        printf("\n1.Create a stack for integers. The maximum stack size is entered from the screen. Find the sum after the maximum element of the stack.");
-        printf("\n2.Create two stacks for integers. The first stack is to organize the input in descending order, the second stack is to organize the input in ascending order. Without sorting and flipping the original stacks, form a third stack ordered in descending order.");
+        printf("\n---------------------------------------------------------------------------------");
+        printf("\n1.Create a stack for integers. The maximum stack size is entered from the screen. \nFind the sum after the maximum element of the stack.");
+        printf("\n---------------------------------------------------------------------------------");
+        printf("\n2.Create two stacks for integers.\nThe first stack is to organize the input in descending order, the second stack is \nto organize the input in ascending order. Without sorting and flipping the \noriginal stacks, form a third stack ordered in descending order.");
+        printf("\n---------------------------------------------------------------------------------");
         printf("\nChoose task: ");
         choose = input(1, 2);
         switch (choose) {
