9039776542b1892a1d3e237e3acc09fc113caac9
Errerp <113059470+Errerp@users.noreply.github.com>
2023-04-04 17:41:56 +0300

    Update func.h

--- Sem 2/Lab 5/func.h
+++ Sem 2/Lab 5/func.h
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
+enum bool{
+    false, true
+};
 struct FILO {
     int data;
     struct FILO *next;
@@ -12,5 +14,10 @@ struct FILO *push(struct FILO* , int num);
 void print_stack(struct FILO* );
 struct FILO *find_max(struct FILO *);
 int find_sum(struct FILO *);
+int check_ask(struct FILO *, int a);
+int check_desc(struct FILO *, int a);
+int cnt_FILO(struct FILO *);
+struct FILO *move_to_n(struct FILO *, int n);
+struct FILO *merge_FILO(struct FILO *, struct FILO *, struct FILO *);
 void task1();
 void task2();
