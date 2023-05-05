48861b607f5db240f4e9c20b9b4bbdbdc000f330
Errerp <113059470+Errerp@users.noreply.github.com>
2023-05-05 19:40:57 +0300

    Update func.h

--- Sem 2/Lab 7/func.h
+++ Sem 2/Lab 7/func.h
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum {
+    false, true
+} bool;
+
 struct Tree {
     int data;
     struct Tree *left;
@@ -13,6 +17,10 @@ void add_tree_element(struct Tree **root, int data);
 
 void delete_tree_element(struct Tree *root);
 
-void find_max_independent_set(struct Tree *root);
+void spiral(struct Tree *root);
+
+bool print_level_left(struct Tree *root, int level);
+
+bool print_level_right(struct Tree *root, int level);
 
 void print_tree(struct Tree *root, int spaces);
